6c87dd8722cdd053049a207e536f1eede2e40d0d
Riz Mark Corpuz <rizmark.corpuz@gmail.com>
2024-10-08 01:13:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path='/PortfolioV2' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/skills' element={<Skills />} />
         <Route path='/work' element={<Work />} />
